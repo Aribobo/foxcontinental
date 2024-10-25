17d282b63ac4d787d8e7868aac9c113f37653f90
Aribobo <stanleyemmanuel04@gmail.com>
2024-10-25 01:59:19 +0100

    project env set up

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,7 +1,5 @@
 import mongoose from 'mongoose';
-const MONGODB_URI = "mongodb+srv://consignment:iDwW0egHJDxXNPsf@cluster0.e8sm3.mongodb.net/Logistics?retryWrites=true&w=majority"
-
-const connectDB = async () => mongoose.connect(MONGODB_URI);
+const connectDB = async () => mongoose.connect(process.env.MONGODB_URI);
 
 export default connectDB;
 
